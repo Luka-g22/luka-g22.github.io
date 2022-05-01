c72e9a377811485ef387cf5db9497303f3a39a61
Luca Guerrero <luka222@live.com.ar>
2022-05-01 11:00:11 -0300

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SkillsComponent } from './skills/skills.component';
 import { FooterComponent } from './footer/footer.component';
-import { MenuComponent } from './menu/menu.component';
-import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 @NgModule({
   declarations: [
     AppComponent,
     SkillsComponent,
     FooterComponent,
-    MenuComponent,
-    HomeComponent,
     AboutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'about', component: AboutComponent},
+      {path: 'skills', component: SkillsComponent},
+      {path: 'footer', component: FooterComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
