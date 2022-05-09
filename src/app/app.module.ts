c7b62c091ee49de4802299ba7689760d7d3ca28b
Luca Guerrero <luka222@live.com.ar>
2022-05-08 22:14:19 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppComponent } from './app.component';
 import { SkillsComponent } from './skills/skills.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { InicioComponent } from './inicio/inicio.component';
 @NgModule({
   declarations: [
     AppComponent,
     SkillsComponent,
     FooterComponent,
     AboutComponent,
+    NavbarComponent,
+    ProyectosComponent,
+    InicioComponent,
   ],
   imports: [
     BrowserModule,
